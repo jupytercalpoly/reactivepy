b0b7d79bc917e827b473649af7ba9aceceac3d3f
Declan Kelly <dkelly.home@gmail.com>
2018-08-01 15:51:45 -0700

    Clean up code object, generate single symbol table

--- reactive_kernel/codeObject.py
+++ reactive_kernel/codeObject.py
@@ -1,43 +1,37 @@
 from symtable import symtable
 
+
 class CodeObject:
     def __init__(self, code):
-        self.input_vars = self._find_input_variables(code)
-        self.output_vars = self._find_output_variables(code)
+        self.symbol_table = symtable(code, '<string>', 'exec')
         self.code = code
+        self.input_vars = self._find_input_variables()
+        self.output_vars = self._find_output_variables()
 
-    def _find_input_variables(self, code):
-        input_vars = []
-        symbols = symtable(code, '<string>', 'exec')
-        input_vars = self._find_symbol_tables(symbols, input_vars)
-        return input_vars
+    def _find_input_variables(self):
+        return frozenset(self._find_symbol_tables(self.symbol_table))
 
-    
-    def _find_symbol_tables(self, symbols, input_vars):
-        for i in symbols.get_symbols():
-            if i.is_global() :
-                input_vars.append(i)
-        for a in symbols.get_children() :
-            self._find_symbol_tables(a, input_vars)
-        else :
-            return input_vars
-            
+    def _find_symbol_tables(self, symbols):
+        for sym in symbols.get_symbols():
+            if sym.is_global():
+                yield sym
 
-    def _find_output_variables(self, code):
-        #return one top level defined variable, only including support for one as of now 
-        output_vars = []
-        symbols = symtable(code, '<string>', 'exec')
-        for i in symbols.get_symbols() :
-            if i.is_assigned() :
-                output_vars.append(i)
-        if len(output_vars) == 0:
-            return
-        if len(output_vars) == 1:
-            return output_vars[0]
-        raise MultipleDefinitionsError()
+        for a in symbols.get_children():
+            yield from self._find_symbol_tables(a)
+
+    def _find_output_variables(self):
+        # return one top level defined variable, only including support for one
+        # as of now
+        output_vars = [sym for sym in self.symbol_table.get_symbols()
+                       if sym.is_assigned()]
+
+        if len(output_vars) > 1:
+            raise MultipleDefinitionsError()
+        else:
+            return output_vars
+
 
 class MultipleDefinitionsError(Exception):
-    """ Thrown if more that one variable defined in cell 
+    """ Thrown if more that one variable defined in cell
     """
-    def __init__(self):
-        self.expr = "Attempted to define more than one local variable"
+    pass
