402dc505705004bb7fa0786a253234eefe5924e5
Declan Kelly <dkelly.home@gmail.com>
2018-08-02 12:50:54 -0700

    Wrap all extracted symbols

--- reactive_kernel/code_object.py
+++ reactive_kernel/code_object.py
@@ -1,4 +1,4 @@
-from symtable import symtable
+from symtable import symtable, Symbol
 
 
 class CodeObject:
@@ -14,7 +14,7 @@ class CodeObject:
     def _find_symbol_tables(self, symbols):
         for sym in symbols.get_symbols():
             if sym.is_global():
-                yield sym
+                yield SymbolWrapper(sym)
 
         for a in symbols.get_children():
             yield from self._find_symbol_tables(a)
@@ -22,7 +22,7 @@ class CodeObject:
     def _find_output_variables(self):
         # return one top level defined variable, only including support for one
         # as of now
-        output_vars = [sym for sym in self.symbol_table.get_symbols()
+        output_vars = [SymbolWrapper(sym) for sym in self.symbol_table.get_symbols()
                        if sym.is_assigned()]
 
         if len(output_vars) > 1:
@@ -35,6 +35,28 @@ class CodeObject:
 
     def __eq__(self, other):
         return self.input_vars == other.input_vars
+
+    def __repr__(self):
+        return f"<Code in:{str(self.input_vars)} out:{str(list(self.output_vars))} code:\"{self.code}\">"
+
+
+class SymbolWrapper:
+    """Wrapper for symtable Symbols that performs hashing and equality check by name"""
+
+    def __init__(self, symbol):
+        self._symbol = symbol
+
+    def __getattr__(self, attr):
+        return self._symbol.__getattribute__(attr)
+
+    def __eq__(self, other):
+        return self._symbol.get_name() == other.get_name()
+
+    def __hash__(self):
+        return hash(self._symbol.get_name())
+
+    def __repr__(self):
+        return f'<{self._symbol.get_name()}>'
 
 
 class MultipleDefinitionsError(Exception):
