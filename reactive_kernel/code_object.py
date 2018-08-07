dae4321a7815c13615f517c1d2499b163ea0a026
Declan Kelly <dkelly.home@gmail.com>
2018-08-06 22:45:45 -0700

    Added check for builtin symbols

--- reactive_kernel/code_object.py
+++ reactive_kernel/code_object.py
@@ -1,40 +1,67 @@
-from symtable import symtable
+from symtable import symtable, Symbol
+import builtins as builtins_mod
 
 
 class CodeObject:
     def __init__(self, code):
-        self.input_vars = self._find_input_variables(code)
-        self.output_vars = self._find_output_variables(code)
+        self.symbol_table = symtable(code, '<string>', 'exec')
         self.code = code
+        self.input_vars = self._find_input_variables()
+        self.output_vars = self._find_output_variables()
 
-    def _find_input_variables(self, code):
-        input_vars = []
-        symbols = symtable(code, '<string>', 'exec')
-        input_vars = self._find_symbol_tables(symbols, input_vars)
-        return input_vars
+    def _find_input_variables(self):
+        return list(self._find_symbol_tables(self.symbol_table))
 
-    def _find_symbol_tables(self, symbols, input_vars):
-        for i in symbols.get_symbols():
-            if i.is_global():
-                input_vars.append(i)
+    def _find_symbol_tables(self, symbols):
+        for sym in symbols.get_symbols():
+            if sym.is_global() and not hasattr(builtins_mod, sym.get_name()):
+                yield SymbolWrapper(sym)
+
         for a in symbols.get_children():
-            self._find_symbol_tables(a, input_vars)
-        else:
-            return input_vars
+            yield from self._find_symbol_tables(a)
 
-    def _find_output_variables(self, code):
+    def _find_output_variables(self):
         # return one top level defined variable, only including support for one
         # as of now
-        output_vars = []
-        symbols = symtable(code, '<string>', 'exec')
-        for i in symbols.get_symbols():
-            if i.is_assigned():
-                output_vars.append(i)
-        if len(output_vars) == 0:
-            return
-        if len(output_vars) == 1:
-            return output_vars[0]
-        raise MultipleDefinitionsError()
+        output_vars = [SymbolWrapper(sym) for sym in self.symbol_table.get_symbols()
+                       if sym.is_assigned()]
+
+        if len(output_vars) > 1:
+            raise MultipleDefinitionsError()
+        else:
+            return frozenset(output_vars)
+
+    def __hash__(self):
+        return hash(self.output_vars)
+
+    def __eq__(self, other):
+        if isinstance(other, CodeObject):
+            return self.output_vars == other.output_vars
+        return False
+
+    def __repr__(self):
+        return f"<Code in:{str(self.input_vars)} out:{str(list(self.output_vars))} code:\"{self.code}\">"
+
+
+class SymbolWrapper:
+    """Wrapper for symtable Symbols that performs hashing and equality check by name"""
+
+    def __init__(self, symbol):
+        self._symbol = symbol
+
+    def __getattr__(self, attr):
+        return self._symbol.__getattribute__(attr)
+
+    def __eq__(self, other):
+        if isinstance(other, SymbolWrapper):
+            return self._symbol.get_name() == other.get_name()
+        return False
+
+    def __hash__(self):
+        return hash(self._symbol.get_name())
+
+    def __repr__(self):
+        return f'<{self._symbol.get_name()}>'
 
 
 class MultipleDefinitionsError(Exception):
