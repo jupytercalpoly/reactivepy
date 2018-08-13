4b33ddae189807bd90a31b259f64de4fa920bab9
RichaGadgil <gadgilricha@gmail.com>
2018-08-13 10:07:55 -0700

    Added syntax error traceback support

--- reactive_kernel/execute.py
+++ reactive_kernel/execute.py
@@ -6,6 +6,12 @@ from ast import AST
 import ast
 from typing import List as ListType
 import traceback
+from IPython.core.ultratb import ListTB
+import IPython.core.ultratb as ultratb
+# from IPython.core.compilerop import CachingCompiler, check_linecache_ipython
+import linecache
+import time
+
 
 _assign_nodes = (ast.AugAssign, ast.AnnAssign, ast.Assign)
 _single_targets_nodes = (ast.AugAssign, ast.AnnAssign)
@@ -16,8 +22,37 @@ class ExecutionContext:
         self.namespace = {}
         self.log = log_func
         self.excepthook = sys.excepthook
+        self.InteractiveTB = ultratb.AutoFormattedTB(mode='Plain',
+                                                     color_scheme='LightBG',
+                                                     tb_offset=1,
+                                                     check_cache=self.check_internal_cache,
+                                                     debugger_cls=None)
+        self.SyntaxTB = ultratb.SyntaxTB(color_scheme='NoColor')
+        self.cache = {}
+        self.saved_getline = linecache.getline
+        self.saved_getlines = linecache.getlines
+
+        def wrapped_getline(*args, **kwargs):
+            result = self.saved_getline(*args, **kwargs)
+            if result is None or len(result) == 0:
+                filename, lineno = args
+                return self.cache[filename][2][lineno - 1]
+            return result
+
+        def wrapped_getlines(*args, **kwargs):
+            result = self.saved_getlines(*args, **kwargs)
+            if result is None or len(result) == 0:
+                return self.cache[args[0]][2]
+            return result
+
+        linecache.getline = wrapped_getline
+        linecache.getlines = wrapped_getlines
 
     def run_cell(self, code, name):
+        entry = (len(code), time.time(),
+                 [line+'\n' for line in code.splitlines()], name)
+        linecache.cache[name] = entry
+        self.cache[name] = entry
         code_ast = ast.parse(code, filename=name, mode='exec')
         result = self._run_ast_nodes(code_ast.body, name)
 
@@ -67,20 +102,35 @@ class ExecutionContext:
         try:
             try:
                 exec(code_obj, globals(), self.namespace)
-            except BaseException:
-                etype, value, tb = sys.exc_info()
-
-                traceback.print_exception(etype, value, tb)
-
-                for frame, lineno in traceback.walk_tb(tb):
-                    print(f"Line #: {lineno}")
-                    print(frame.f_lasti)
             finally:
                 # Reset our crash handler in place
                 sys.excepthook = old_excepthook
         except BaseException:
-            pass
+            try:
+                etype, value, tb = sys.exc_info()
+                stb = self.InteractiveTB.structured_traceback(
+                    etype, value, tb
+                )
+
+                if issubclass(etype, SyntaxError):
+                    # If the error occurred when executing compiled code, we should provide full stacktrace
+                    elist = traceback.extract_tb(tb)
+                    stb = self.SyntaxTB.structured_traceback(
+                        etype, value, elist)
+                    print(self.InteractiveTB.stb2text(stb), file=sys.stderr)
+                else:
+
+                    # Actually show the traceback
+                    print(self.InteractiveTB.stb2text(stb), file=sys.stderr)
+
+            # traceback.print_exception(etype, value, tb)
+            except BaseException as e:
+                print(e)
         else:
             outflag = False
 
         return outflag
+
+    def check_internal_cache(self, *args):
+        linecache.checkcache(*args)
+        linecache.cache.update(self.cache)
