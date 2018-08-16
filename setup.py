98f34a9575ab418e8c370841fb3e3050fa5eecd3
Declan Kelly <dkelly.home@gmail.com>
2018-08-16 15:44:42 -0700

    Add comprehensive clean command to setup

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@
 from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
+from setuptools import Command
+import glob
 import argparse
 import json
 import os
 import sys
 from jupyter_client.kernelspec import KernelSpecManager
 from IPython.utils.tempdir import TemporaryDirectory
+import shutil
 
 kernel_json = {
     'argv': [sys.executable, '-m', 'reactivepy', '-f', '{connection_file}'],
@@ -42,6 +45,9 @@ def _is_root():
         return os.geteuid() == 0
     except AttributeError:
         return False
+
+
+HERE = os.path.realpath(os.path.dirname(__file__))
 
 
 def calculate_user_prefix(options):
@@ -99,6 +105,38 @@ class PostInstallCommand(install):
         install_kernel_spec(user=user, prefix=prefix)
 
 
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        global HERE
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob.glob(
+                os.path.normpath(
+                    os.path.join(
+                        HERE, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(HERE):
+                    # Die if path in CLEAN_FILES is absolute + outside this
+                    # directory
+                    raise ValueError(
+                        "%s is not a path inside %s" %
+                        (path, HERE))
+                print('removing %s' % os.path.relpath(path))
+                shutil.rmtree(path)
+
+
 setup(name='reactivepy',
       version='0.1.0',
       description='Reactive Kernel for Jupyter',
@@ -108,10 +146,9 @@ setup(name='reactivepy',
       packages=['reactivepy'],
       license='BSD 3-Clause License',
       requires=[
-          'ipython>=4.0.0',
-          'jupyter_client',
-          'tornado>=4.0',
-          'ipykernel>=4.8'
+          'ipython',
+          'jupyter_client', 'tornado',
+          'ipykernel'
       ],
       install_requires=[
           'ipython>=4.0.0',
@@ -127,5 +164,6 @@ setup(name='reactivepy',
       cmdclass={
           'develop': PostDevelopCommand,
           'install': PostInstallCommand,
+          'clean': CleanCommand
       },
       )
