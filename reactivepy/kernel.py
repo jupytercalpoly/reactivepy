3cdae79b180bd4ce810e3a7f5933526957cd2bec
Declan Kelly <dkelly.home@gmail.com>
2018-08-20 19:17:35 -0700

    Move execution into schedule queue
    Also support early forms of async generators and regular generators
    
    Also fix support for pinning variable declaration to specific cells

--- reactivepy/kernel.py
+++ reactivepy/kernel.py
@@ -1,7 +1,7 @@
 from ipykernel.kernelbase import Kernel
 import sys
 from .code_object import CodeObject, SymbolWrapper
-from .execute import Executor
+from .execute import Executor, ExecutionResult, CapturedIOCtx
 from .dependencies import DependencyTracker
 import traceback as tb
 from typing import Union, Any, Dict, FrozenSet
@@ -11,12 +11,16 @@ from importlib.abc import InspectLoader
 from .transactional import TransactionDict, TransactionalABC
 from tornado.ioloop import IOLoop
 from asyncio.locks import Lock
+from asyncio import Queue
+import asyncio
 import time
 from ipython_genutils import py3compat
 from ipykernel.jsonutil import json_clean
 from functools import partial
+from typing import Union, Tuple, List
 import random
 import string
+import inspect
 
 __version__ = '0.1.0'
 
@@ -39,12 +43,6 @@ class ExecutionUnitInfo:
         if isinstance(other, ExecutionUnitInfo):
             return self.code_obj.display_id == other.code_obj.display_id
         return False
-
-    def pin(self, cell_id: str):
-        if self.is_pinned:
-            raise RedefiningOwnedCellException
-        else:
-            self.pinning_cell = cell_id
 
     def unpin(self):
         self.pinning_cell = None
@@ -166,18 +164,41 @@ class RequestInfo:
         self.parent = parent
         self.ident = ident
         content = self.content = parent[u'content']
+        header = self.header = parent['header']
         self.code = py3compat.cast_unicode_py2(content[u'code'])
         self.silent = content[u'silent']
         self.store_history = content.get(u'store_history', not self.silent)
         self.user_expressions = content.get('user_expressions', {})
         self.allow_stdin = content.get('allow_stdin', False)
+        self.metadata = parent[u'metadata']
+        self.msg_id = header['msg_id']
+        self.msg_type = header['msg_type']
 
         # Fields to populate later
-        self.metadata = None
         self.execution_count = None
 
-    def send_options(self):
-        return {'parent': self.parent}
+
+class ExecBlock:
+    COUNTER = 0
+
+    def __init__(self, request: RequestInfo, current_exec_unit: ExecutionUnitInfo,
+                 descendant_exec_units: List[ExecutionUnitInfo], async_gen_obj=None):
+        self.current = current_exec_unit
+        self.descendants = descendant_exec_units
+        self.request = request
+
+        # Is this block of execution from a previous iteration
+        self.is_iter_exec = async_gen_obj is not None
+        self.async_gen_obj = async_gen_obj
+
+        ExecBlock.COUNTER += 1
+        self.id = ExecBlock.COUNTER
+
+
+class AsyncGenContainer:
+    def __init__(self):
+        # Mapping from target id (variable name) to current async generator
+        self.generators = dict()
 
 
 class ReactivePythonKernel(Kernel):
@@ -191,6 +212,10 @@ class ReactivePythonKernel(Kernel):
         'file_extension': '.py'
     }
     banner = ''
+
+    # measured in seconds. This value is intended to delay the same amount as
+    # 60fps does between frames
+    REGULAR_GENERATOR_DELAY = 0.016666
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -207,6 +232,178 @@ class ReactivePythonKernel(Kernel):
                                                 color_scheme='LightBG',
                                                 tb_offset=1,
                                                 debugger_cls=None)
+        self._execution_queue = Queue(loop=self._eventloop.asyncio_loop)
+        # mapping from variable name (target id) to (request id, generator
+        # object)
+        self._registered_generators = dict()
+
+        self._eventloop.spawn_callback(
+            self._execution_loop)
+
+    async def _run_single_async_iter_step(self, item, exec_unit):
+        # Make sure only single variable is important for execution
+        # unit
+        assert len(exec_unit.code_obj.output_vars) == 1
+        target_id = list(
+            exec_unit.code_obj.output_vars)[0].get_name()
+
+        request_id, async_gen_obj, insert_id = self._registered_generators[
+            target_id]
+        async with self._inner_state_lock:
+            if request_id != item.request.msg_id and insert_id < item.id:
+                # self._log(item.request,
+                #           f"Cancelling ({request_id}, {insert_id}) for ({item.request.msg_id}, {item.id}) 1")
+                await async_gen_obj.aclose()
+
+                self._registered_generators[target_id] = item.request.msg_id, item.async_gen_obj, item.id
+            elif insert_id > item.id:
+                return None
+
+        try:
+            exec_result = await self._execution_ctx.run_coroutine(anext(item.async_gen_obj), target_id, nohandle_exceptions=(StopAsyncIteration, ))
+        except StopAsyncIteration as e:
+            # iteration is complete
+            raise e
+
+        # Schedule next instance of iterator
+        # Compute execution units which depend on this unit,
+        # and schedule a new block to run which contains them
+        descendants = self._dep_tracker.get_descendants(
+            exec_unit.display_id)
+        descendant_exec_units = list(
+            map(lambda display_id: self._exec_unit_container.get_by_display_id(display_id), descendants))
+
+        next_iter_round = ExecBlock(
+            item.request,
+            exec_unit,
+            descendant_exec_units,
+            async_gen_obj=item.async_gen_obj)
+        await self._execution_queue.put(next_iter_round)
+
+        return exec_result
+
+    async def _first_async_iter_step(self, item: ExecBlock, exec_unit: ExecutionUnitInfo, exec_result: ExecutionResult):
+        # self._log(item.request, "Before inner state lock")
+        async with self._inner_state_lock:
+            # self._log(item.request, "After inner state lock")
+            if exec_result.target_id in self._registered_generators:
+                request_id, async_gen_obj, insert_id = self._registered_generators[
+                    exec_result.target_id]
+
+                if insert_id < item.id:
+                    # self._log(
+                    #     item.request, f"Cancelling ({request_id}, {insert_id}) for ({item.request.msg_id}, {item.id}) 2")
+
+                    await async_gen_obj.aclose()
+                else:
+                    raise ValueError(
+                        f"Very confuse. {insert_id} vs {item.id}")
+
+            # self._log(
+            #     item.request,
+            #     f"Storing {(item.request.msg_id, exec_result.output, item.id)}")
+            self._registered_generators[exec_result.target_id] = item.request.msg_id, exec_result.output, item.id
+
+        # Get first value and replace async gen with first
+        # value or None
+        async_gen_obj = exec_result.output
+        try:
+            exec_result.output = await anext(exec_result.output)
+        except Exception:
+            # DO not schedule further updates on failure
+            return False
+
+        # Schedule next instance of iterator
+        # Compute execution units which depend on this unit,
+        # and schedule a new block to run which contains
+        # them
+        descendants = self._dep_tracker.get_descendants(
+            exec_unit.display_id)
+        descendant_exec_units = list(
+            map(lambda display_id: self._exec_unit_container.get_by_display_id(display_id), descendants))
+        next_iter_round = ExecBlock(
+            item.request, exec_unit, descendant_exec_units, async_gen_obj=async_gen_obj)
+
+        await self._execution_queue.put(next_iter_round)
+
+        return True
+
+    async def _execution_loop(self):
+        while True:
+            item: ExecBlock = await self._execution_queue.get()
+            # self._log(item.request, f"Handling {item.request.msg_id}")
+            if item is None:
+                # Use None to signal end
+                # self._log(item.request, 'Breaking execution loop!')
+                break
+
+            for exec_unit in ([item.current] + item.descendants):
+                # async with self._execution_lock:
+                # self._log(item.request, exec_unit.display_id)
+
+                is_current = exec_unit == item.current
+                is_iter = item.is_iter_exec and is_current
+
+                if is_iter:
+                    # self._log(item.request, "Stepping iter")
+
+                    try:
+                        exec_result = await self._run_single_async_iter_step(item, exec_unit)
+                    except StopAsyncIteration:
+                        # self._log(item.request,
+                        #           'Step failed, breaking exec unit loop!')
+                        break
+                else:
+                    # self._log(item.request, "Executing normally")
+                    # Execute code normally
+                    exec_result = self._execution_ctx.run_cell(
+                        exec_unit.code_obj.code, exec_unit.display_id)
+
+                if exec_result.output is not None and not is_iter:
+                    is_awaitable, is_gen, is_async_gen = inspect_output_attrs(
+                        exec_result.output)
+                    # self._log(item.request,
+                    #           f"{is_awaitable} {is_gen} {is_async_gen}")
+
+                    # These flags should be disjoint (never multiple True at same time)
+                    # but this layout allows for transforming a regular generator into an async generator
+                    # with a small timeout, and then passing it to the
+                    # async_gen branch
+
+                    # If the output is awaitable, wait for it and then replace
+                    # the old output
+                    if is_awaitable:
+                        exec_result.output = await exec_result.output
+
+                    # If the output is a regular generator, wrap it in an async
+                    # generator that will add a very small delay
+                    if is_gen:
+                        exec_result.output = convert_gen_to_async(
+                            exec_result.output, ReactivePythonKernel.REGULAR_GENERATOR_DELAY)()
+                        is_async_gen = True
+                        is_gen = False
+
+                    # If it is an async generator, either replace the old
+                    # generator that was active for this target name or
+                    # register this completely new generator. Remove the old
+                    # generator by canceling it
+                    if is_async_gen:
+                        # self._log(item.request, "Is now async gen!")
+                        setup_succeeded = await self._first_async_iter_step(
+                            item, exec_unit, exec_result)
+
+                        if not setup_succeeded:
+                            break
+
+                    if is_awaitable or is_gen or is_async_gen:
+                        self._execution_ctx.update_ns(
+                            {exec_result.target_id: exec_result.output})
+
+                if not item.request.silent:
+                    self._output_exec_results(
+                        exec_unit, item.request, is_current and not is_iter, exec_result)
+
+            self._execution_queue.task_done()
 
     def _update_existing_exec_unit(self, code_obj, cell_id):
         # 4a. If it is a redefinition, get the old execution unit
@@ -268,7 +465,7 @@ class ReactivePythonKernel(Kernel):
                 self._dep_tracker.add_edge(
                     code_object_id, code_obj.display_id)
             else:
-                raise DefinitionNotFoundException()
+                raise DefinitionNotFoundException(str(sym))
 
         return current_exec_unit
 
@@ -291,7 +488,7 @@ class ReactivePythonKernel(Kernel):
 
         stop_on_error = request.content.get('stop_on_error', True)
 
-        request.metadata = self.init_metadata(parent)
+        response_meta = self.init_metadata(parent)
 
         reply_content = await self.do_execute(request)
 
@@ -306,11 +503,11 @@ class ReactivePythonKernel(Kernel):
 
         # Send the reply.
         reply_content = json_clean(reply_content)
-        request.metadata = self.finish_metadata(
-            parent, request.metadata, reply_content)
+        response_meta = self.finish_metadata(
+            parent, response_meta, reply_content)
 
         reply_msg = self.session.send(stream, u'execute_reply',
-                                      reply_content, parent, metadata=request.metadata,
+                                      reply_content, parent, metadata=response_meta,
                                       ident=ident)
 
         self.log.debug("%s", reply_msg)
@@ -325,12 +522,13 @@ class ReactivePythonKernel(Kernel):
             self._inner_execute_request_callback,
             stream, ident, parent)
 
-    def _update_kernel_state(self, code_obj, cell_id, deleted_cell_ids):
+    def _update_kernel_state(self, code_obj,
+                             cell_id, deleted_cell_ids):
         # 3. If deletedCells was passed, then unpin all execution units
         # that were previously attached to a cell
         if deleted_cell_ids is not None:
-            for cell_id in deleted_cell_ids:
-                self._exec_unit_container.unpin_exec_unit(cell_id)
+            for to_delete_cell_id in deleted_cell_ids:
+                self._exec_unit_container.unpin_exec_unit(to_delete_cell_id)
 
         self._dep_tracker.start_transaction()
         self._exec_unit_container.start_transaction()
@@ -354,26 +552,22 @@ class ReactivePythonKernel(Kernel):
             self._dep_tracker.commit()
             self._exec_unit_container.commit()
 
-        # 5. Compute the dependant execution units which must be rerun, and
-        # add the current execution unit to the front of the list
-        descendants = self._dep_tracker.get_descendants(
-            code_obj.display_id)
-        to_run = [current_exec_unit] + list(
-            map(lambda display_id: self._exec_unit_container.get_by_display_id(display_id), descendants))
-
-        return to_run, current_exec_unit
+        return current_exec_unit
 
     def _output_exec_results(
-            self, exec_unit, request: RequestInfo, is_not_current, stdout, stderr, output):
+            self, exec_unit: ExecutionUnitInfo, request: RequestInfo, is_current: bool, exec_result: ExecutionResult):
+
         # 6b. Determine whether the current execution unit will be
         # directly display or update
-        message_mode = 'update_display_data' if is_not_current else 'display_data'
+        message_mode = 'update_display_data' if not is_current else 'display_data'
 
         # 6c. Create rich outputs for captured output
-        if output is not None:
-            data, md = self.formatter.format(output)
+        if exec_result.output is not None:
+            data, md = self.formatter.format(exec_result.output)
         else:
             data, md = {}, {}
+
+        stdout, stderr = exec_result.stdout.getvalue(), exec_result.stderr.getvalue()
 
         # 6d. For the captured output value, stdout, and stderr
         # send appropriate responses back to the front-end
@@ -388,7 +582,7 @@ class ReactivePythonKernel(Kernel):
                 }
             }, parent=request.parent)
 
-        if output is not None:
+        if exec_result.output is not None:
             self.session.send(self.iopub_socket, message_mode, content={
                 'data': data,
                 'metadata': md,
@@ -413,24 +607,28 @@ class ReactivePythonKernel(Kernel):
             code_obj = CodeObject(request.code, self._key)
 
             # 2. Extract metadata (both are optional)
-            cell_id = request.metadata['cellId'] if 'cellID' in request.metadata else None
+            cell_id = request.metadata['cellId'] if 'cellId' in request.metadata else None
             deleted_cell_ids = request.metadata[
                 'deletedCells'] if 'deletedCells' in request.metadata else None
 
+            # Doesn't need lock because the code inside _update_kernel_state
+            # will never await. If that ever changes, uncomment lock below
             async with self._inner_state_lock:
-                to_run, current_exec_unit = self._update_kernel_state(
+                current_exec_unit = self._update_kernel_state(
                     code_obj, cell_id, deleted_cell_ids)
 
-            async with self._execution_lock:
-                # 6. For each execution unit which must be run
-                for exec_unit in to_run:
-                    # 6a. Run the code and capture everything written to stdout,
-                    # stderr, and the displayhook
-                    exec_result = await self._execution_ctx.run_cell(exec_unit.code_obj.code, exec_unit.display_id)
+                # 5. Compute the dependant execution units which must be rerun, and
+                # add the current execution unit to the front of the list
+                descendants = self._dep_tracker.get_descendants(
+                    code_obj.display_id)
+                descendant_exec_units = list(
+                    map(lambda display_id: self._exec_unit_container.get_by_display_id(display_id), descendants))
 
-                    if not request.silent:
-                        self._output_exec_results(
-                            exec_unit, request, exec_unit != current_exec_unit, exec_result.stdout.getvalue(), exec_result.stderr.getvalue(), exec_result.output)
+            block = ExecBlock(
+                request,
+                current_exec_unit,
+                descendant_exec_units)
+            await self._execution_queue.put(block)
 
         except Exception:
             etype, value, tb = sys.exc_info()
@@ -446,7 +644,7 @@ class ReactivePythonKernel(Kernel):
                     'traceback': formatted_lines}
             if not request.silent:
                 self.session.send(self.iopub_socket,
-                                   'error', content=error_content, parent=request.parent)
+                                  'error', content=error_content, parent=request.parent)
             error_content['status'] = 'error'
 
             return error_content
@@ -454,3 +652,42 @@ class ReactivePythonKernel(Kernel):
         return {'status': 'ok',
                 'execution_count': request.execution_count,
                 }
+
+
+async def anext(*args):
+    if len(args) < 1:
+        raise TypeError(
+            f"anext expected at least 1 arguments, got {len(args)}")
+
+    aiterable, default, has_default = args[0], None, False
+
+    if len(args) > 2:
+        raise TypeError(f"anext expected at most 2 arguments, got {len(args)}")
+
+    if len(args) == 2:
+        default = args[1]
+        has_default = True
+
+    try:
+        return await aiterable.__anext__()
+    except StopAsyncIteration as exc:
+        if has_default:
+            return default
+        raise StopAsyncIteration() from exc
+
+
+def inspect_output_attrs(obj):
+    is_awaitable = inspect.isawaitable(obj)
+    is_gen = inspect.isgenerator(obj)
+    is_async_gen = inspect.isasyncgen(obj)
+
+    return (is_awaitable, is_gen, is_async_gen)
+
+
+def convert_gen_to_async(gen, delay):
+    async def inner_async_gen():
+        for value in gen:
+            yield value
+            await asyncio.sleep(delay)
+
+    return inner_async_gen
